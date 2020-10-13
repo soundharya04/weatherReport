94e45acab20af7840ea724373e29252b02fdc267
soundharya04 <souns14@gmail.com>
2020-10-13 10:55:44 +0530

    Updates

---
+++ precache-manifest.2122f7250bfbbec4c53d9ef477b7e9bb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec6faca692dab41800575f7ccf17da9",
+    "url": "/weather_report/index.html"
+  },
+  {
+    "revision": "b72240a5a8480b614cf7",
+    "url": "/weather_report/static/css/main.d56f8b9b.chunk.css"
+  },
+  {
+    "revision": "b1f76098c01530f28d84",
+    "url": "/weather_report/static/js/0.6040b5d0.chunk.js"
+  },
+  {
+    "revision": "444101d093a093ffe1da",
+    "url": "/weather_report/static/js/3.5e4caf0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_report/static/js/3.5e4caf0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "180f8af430afdb802f60",
+    "url": "/weather_report/static/js/4.5651686c.chunk.js"
+  },
+  {
+    "revision": "b72240a5a8480b614cf7",
+    "url": "/weather_report/static/js/main.7cb3d6a8.chunk.js"
+  },
+  {
+    "revision": "c3f10dfec3faa7467238",
+    "url": "/weather_report/static/js/runtime-main.ff21b2cc.js"
+  },
+  {
+    "revision": "ddd54bb4518e74089cccde1402447eba",
+    "url": "/weather_report/static/media/Humidity.ddd54bb4.jpg"
+  },
+  {
+    "revision": "b0bfa411209e86a3fb20a4395b7aab5a",
+    "url": "/weather_report/static/media/Pressure.b0bfa411.jpg"
+  },
+  {
+    "revision": "0d7934c2dd2dc36b8d71e1a0f83fd0a1",
+    "url": "/weather_report/static/media/Temperature.0d7934c2.jpg"
+  },
+  {
+    "revision": "c016881316d71a26ce2fe401ecdfdb22",
+    "url": "/weather_report/static/media/Wind.c0168813.jpg"
+  }
+]);
